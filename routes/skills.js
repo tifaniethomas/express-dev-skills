9a69d8d0bc23cc34fd562d22f4d517ddb9f290f3
Tifanie Thomas <tifanie.n.thomas@gmail.com>
2023-08-15 16:40:34 -0500

    update skills in routes

--- routes/skills.js
+++ routes/skills.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const skillsCtrl = require('../controllers/skills')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// All actual paths start with '/skills'
+
+router.get('/', skillsCtrl.index);
+
+//GET /skills/:id
+router.get('/:id', skillsCtrl.show)
 
 module.exports = router;
