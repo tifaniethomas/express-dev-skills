e148865b62e2f232f2dd8421085958b91263a8fb
Tifanie Thomas <tifanie.n.thomas@gmail.com>
2023-08-15 19:52:01 -0500

    refactor views with EJS partial templates

--- controllers/skills.js
+++ controllers/skills.js
@@ -7,12 +7,14 @@ module.exports = {
 
   function show(req, res) {
     res.render('skills/show', {
-      skill: Skill.getOne(req.params.id)
+      skill: Skill.getOne(req.params.id),
+      title: 'Skill Details'
     })
   }
 	
   function index(req, res) {
     res.render('skills/index', {
-      skills: Skill.getAll()
+      skills: Skill.getAll(),
+      title: 'All Skills'
     });
   }
